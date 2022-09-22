bd08b85115631c4be2e683abfcff7be570d94b2a
Callum <callumjrobinson2020@gmail.com>
2022-09-22 17:54:00 +0100

    Simple cmath example

---
+++ Functions/CmathExample/CmathExample/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+	Simple example using the log2 function from cmath
+*/
+int main() {
+	double answer;
+	answer = log2(512);
+
+	cout << "What power of 2 will get 512??" << endl;
+	cout << "The answer is: " << answer << endl;
+}
